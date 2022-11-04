e631fad8a53722558fce0e609e4196bda8a4e6ec
Bruna Terribile <brunaterribile@gmail.com>
2022-11-04 13:17:38 -0300

    Refactor

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
 function createGame(team1, time, team2){
   return `
   <li>
-    <img src="./assets/icon-${team1}.svg" alt="Bandeira do ${team1}" />
+    <img src="./assets/icon=${team1}.svg" alt="Bandeira do ${team1}" />
     <strong>${time}</strong>
-    <img src="./assets/icon-${team2}.svg" alt="Bandeira da ${team2}" />
+    <img src="./assets/icon=${team2}.svg" alt="Bandeira da ${team2}" />
   </li>
   `
 }
@@ -20,14 +19,7 @@ function createCard(date, day, games){
   `
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="Logo da NLW" />
-  </header>
-
-  <main id="cards">
-    ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
-    ${createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland"))}
-    ${createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))}
-  </main>
-`
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland")) +
+  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
